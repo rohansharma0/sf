e26d1338a52845bcb9c087b3ef80f06e709a20a0
Rohan Sharma <iamrohan29@gmail.com>
2025-08-15 01:17:20 +0530

    Fix deployment build issue

--- src/auth/passport.ts
+++ src/auth/passport.ts
@@ -1,18 +1,18 @@
-import passport from "passport";
-import { googleStrategy } from "./google.strategy";
-import User from "../models/user.model";
+// import passport from "passport";
+// import { googleStrategy } from "./google.strategy";
+// import User from "../models/user.model";
 
-export const configurePassport = () => {
-    googleStrategy(passport);
+// export const configurePassport = () => {
+//     googleStrategy(passport);
 
-    passport.serializeUser((user: any, done) => done(null, user.id));
+//     passport.serializeUser((user: any, done) => done(null, user.id));
 
-    passport.deserializeUser(async (id, done) => {
-        try {
-            const user = await User.findById(id);
-            done(null, user);
-        } catch (err) {
-            done(err, null);
-        }
-    });
-};
+//     passport.deserializeUser(async (id, done) => {
+//         try {
+//             const user = await User.findById(id);
+//             done(null, user);
+//         } catch (err) {
+//             done(err, null);
+//         }
+//     });
+// };
